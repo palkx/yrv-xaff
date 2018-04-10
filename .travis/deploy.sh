4751139499fb48cfca0aae51389364880afc54b7
Mikhail K <sm1leua@ya.ru>
2018-04-11 00:58:10 +0300

    Testing auto deploy #5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,8 @@
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 pwd # Echo current directory for testing purposes
-ls -l # Directory structure for testing purposes
+ls -al # Directory structure for testing purposes
+ls -al .travis
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
