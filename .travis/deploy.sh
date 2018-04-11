cbc032767eee663f7b42923b8be53511742490e1
Mikhail K <sm1leua@ya.ru>
2018-04-11 03:06:38 +0300

    Auto deploy #12

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,13 +4,12 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$TEMP_DEPLOY_DIR
-git push deploy master
-
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh deploy@$IP -p $PORT <<EOF
+  rm -rf $TEMP_DEPLOY_DIR
+  mkdir $TEMP_DEPLOY_DIR
   cd $TEMP_DEPLOY_DIR
+  git clone git@github.com:iSm1le/yrv-xaff.git .
   npm install
   npm run build
   rm -rf $DEPLOY_DIR/*
