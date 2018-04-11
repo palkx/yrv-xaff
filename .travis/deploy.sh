d64011f580da47654340dfdc9fa12b2e20769142
Mikhail K <sm1leua@ya.ru>
2018-04-11 03:39:10 +0300

    Auto deploy #15

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,9 +9,7 @@ ssh deploy@$IP -p $PORT <<EOF
   rm -rf $TEMP_DEPLOY_DIR
   mkdir $TEMP_DEPLOY_DIR
   cd $TEMP_DEPLOY_DIR
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
-  git clone -b production git@github.com:iSm1le/yrv-xaff.git .
+  git clone -b production https://github.com/iSm1le/yrv-xaff.git .
   npm install
   npm run build
   rm -rf $DEPLOY_DIR/*
