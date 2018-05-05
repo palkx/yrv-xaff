8528f062d84c3cb52b2ac54d9f27c5c8f8498460
Mikhail K <sm1leua@ya.ru>
2018-05-05 15:05:10 +0300

    Added volume setting

---
+++ src/settings/index.js
@@ -0,0 +1,49 @@
+export default {
+  settings: {
+    volume: 50
+  },
+
+
+  /**
+   * @param {number} volume Volume that saved in user localStorage
+   * @returns {void}
+   */
+  setVolume(volume) {
+    console.log(`Updating volume to ${volume}`);
+    this.settings.volume = volume;
+    this.updateSettings(this.settings);
+  },
+
+  /**
+   * @returns {number} User player volume
+   */
+  getVolume() {
+    this.loadSettings();
+    return this.settings.volume;
+  },
+
+  /**
+   * @returns {array} User settings
+   */
+  getSettings() {
+    this.loadSettings();
+    return this.settings;
+  },
+
+  /**
+   * @param {array} settings Stores user settings in localStorage
+   */
+  updateSettings(settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
+  },
+
+  /**
+   * Loads settings from localStorage
+   */
+  loadSettings() {
+    if (localStorage.getItem('settings')) {
+      const settings = JSON.parse(localStorage.getItem('settings'));
+      this.settings = settings;
+    }
+  }
+};
